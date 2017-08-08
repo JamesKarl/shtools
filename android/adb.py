c51b930a3283c30eb458b5990076b5a46b7db2b2
James <jameskarl2@163.com>
2017-08-08 18:14:43 +0800

    extras must be passed separately

--- android/adb.py
+++ android/adb.py
@@ -11,18 +11,21 @@ def run(cmd):
 
 def start_activity(params, activity=".visit.offline.ui.Base64ImageCategoryListActivity", package="com.wiseda.hbzy"):
     extras = ""
-    for k in params.keys():
-        extras += " -e " + k + " " + params[k] + " "
+    for it in params:
+        extras += " " + it + " "
 
     run(ADB + "shell am start -W " + extras + " -n " + package + "/" + activity)
 
 
 def main():
     #店铺陈列
-    #start_activity({"BUSINESS_ID": "4f301e41b78340f38cfc970dcef7e7a9"})
+    #start_activity({"-e BUSINESS_ID": "4f301e41b78340f38cfc970dcef7e7a9"})
 
     #喜庆活动
-    start_activity({"BUSINESS_ID": "ace8a08a8feb4df5af1a07ada9a4113a"}, ".visit.offline.ui.Base64ImageListActivity")
+    start_activity(["-e BUSINESS_ID ace8a08a8feb4df5af1a07ada9a4113a",
+                    "-e BUSINESS_TYPE CELEBRATION",
+                    "--ez EDITABLE true"
+                    ], ".visit.offline.ui.Base64ImageListActivity")
 
 
 if __name__ == '__main__':
